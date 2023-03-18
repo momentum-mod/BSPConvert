960403908e8f01417f9f0e6ad521fd5e24df6c59
Tyler Befferman <TylerBeff@gmail.com>
2023-03-18 11:26:12 -0600

    Improved help text Removed --oldbsp option

--- BSPConversionCmd/Program.cs
+++ BSPConversionCmd/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BSPConversionLib;
 using CommandLine;
+using CommandLine.Text;
 
 namespace BSPConversionCmd
 {
@@ -16,17 +17,17 @@ namespace BSPConversionCmd
 			[Option("mindmg", Required = false, Default = 50, HelpText = "Minimum damage to convert trigger_hurt into trigger_teleport.")]
 			public int MinDamageToConvertTrigger { get; set; }
 
-			[Option("oldbsp", Required = false, HelpText = "Use BSP version 20.")]
-			public bool OldBSP { get; set; }
+			//[Option("oldbsp", Required = false, HelpText = "Use BSP version 20 (HL:2 / CS:S).")]
+			//public bool OldBSP { get; set; }
 
 			[Option("prefix", Required = false, HelpText = "Prefix for the converted BSP's file name.")]
 			public string Prefix { get; set; }
 
+			[Option("output", Required = false, HelpText = "Output game directory for converted BSP/materials.")]
+			public string OutputDirectory { get; set; }
+
 			[Value(0, MetaName = "input files", Required = true, HelpText = "Input Quake 3 BSP/PK3 file(s) to be converted.")]
 			public IEnumerable<string> InputFiles { get; set; }
-
-			[Option('o', "output", Required = false, HelpText = "Output game directory for converted BSP/materials.")]
-			public string OutputDirectory { get; set; }
 		}
 
 		static void Main(string[] args)
@@ -37,31 +38,58 @@ namespace BSPConversionCmd
 			//	"--output", @"c:\users\tyler\documents\tools\source engine\bspconvert\output",
 			//};
 
-			Parser.Default.ParseArguments<Options>(args)
-				.WithParsed(options =>
+			var parser = new Parser(with => with.HelpWriter = null);
+			var parserResult = parser.ParseArguments<Options>(args);
+			parserResult
+				.WithParsed(options => RunCommand(options))
+				.WithNotParsed(errors => DisplayHelp(errors, parserResult));
+		}
+
+		static void RunCommand(Options options)
+		{
+			if (options.DisplacementPower < 2 || options.DisplacementPower > 4)
+				throw new ArgumentOutOfRangeException("Displacement power must be between 2 and 4.");
+
+			if (options.OutputDirectory == null)
+				options.OutputDirectory = Path.GetDirectoryName(options.InputFiles.First());
+
+			foreach (var inputEntry in options.InputFiles)
+			{
+				var converterOptions = new BSPConverterOptions()
 				{
-					if (options.DisplacementPower < 2 || options.DisplacementPower > 4)
-						throw new ArgumentOutOfRangeException("Displacement power must be between 2 and 4.");
+					noPak = options.NoPak,
+					DisplacementPower = options.DisplacementPower,
+					minDamageToConvertTrigger = options.MinDamageToConvertTrigger,
+					//oldBSP = options.OldBSP,
+					prefix = options.Prefix,
+					inputFile = inputEntry,
+					outputDir = options.OutputDirectory
+				};
+				var converter = new BSPConverter(converterOptions, new ConsoleLogger());
+				converter.Convert();
+			}
+		}
 
-					if (options.OutputDirectory == null)
-						options.OutputDirectory = Path.GetDirectoryName(options.InputFiles.First());
+		static void DisplayHelp(IEnumerable<Error> errors, ParserResult<Options> parserResult)
+		{
+			const string version = "BSP Convert 0.0.1-alpha";
+			if (errors.IsVersion())
+			{
+				Console.WriteLine(version);
+				return;
+			}
+			
+			var helpText = HelpText.AutoBuild(parserResult, h =>
+			{
+				h.AdditionalNewLineAfterOption = false;
+				h.MaximumDisplayWidth = 400;
+				h.Heading = version;
+				h.Copyright = "";
+				h.AddPostOptionsLine("EXAMPLE:\n  .\\BSPConv.exe \"C:\\Users\\<username>\\Documents\\BSPConvert\\nood-aDr.pk3\" --output \"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Momentum Mod Playtest\\momentum\" --prefix \"df_\"");
 
-					foreach (var inputEntry in options.InputFiles)
-					{
-						var converterOptions = new BSPConverterOptions()
-						{
-							noPak = options.NoPak,
-							DisplacementPower = options.DisplacementPower,
-							minDamageToConvertTrigger = options.MinDamageToConvertTrigger,
-							oldBSP = options.OldBSP,
-							prefix = options.Prefix,
-							inputFile = inputEntry,
-							outputDir = options.OutputDirectory
-						};
-						var converter = new BSPConverter(converterOptions, new ConsoleLogger());
-						converter.Convert();
-					}
-				});
+				return HelpText.DefaultParsingErrorsHandler(parserResult, h);
+			}, e => e);
+			Console.WriteLine(helpText);
 		}
 	}
 }
