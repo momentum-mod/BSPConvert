1a43af04324f901256e451a8fe1e36816aaf0fc0
Tyler Befferman <TylerBeff@gmail.com>
2023-04-28 21:07:14 -0600

    Added unit test project

---
+++ BSPConvert.Test/BSPConverterTest.cs
@@ -0,0 +1,50 @@
+using BSPConvertLib;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace BSPConvert.Test
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+			// Clear "Converted" folder
+			var outputDir = Path.Combine(TestContext.CurrentContext.TestDirectory, "Converted");
+			if (Directory.Exists(outputDir))
+				Directory.Delete(outputDir, true);
+
+			Directory.CreateDirectory(outputDir);
+		}
+
+		[Test]
+		public void ConvertTestFiles()
+		{
+			var testFilesDir = Path.Combine(TestContext.CurrentContext.TestDirectory, "Test Files");
+			var files = Directory.GetFiles(testFilesDir, "*.bsp", SearchOption.AllDirectories);
+			foreach (var file in files)
+				Convert(file);
+			
+			Assert.Pass();
+		}
+
+		private void Convert(string bspFile)
+		{
+			var outputDir = Path.Combine(TestContext.CurrentContext.TestDirectory, "Converted");
+			var options = new BSPConverterOptions()
+			{
+				noPak = false,
+				DisplacementPower = 4,
+				minDamageToConvertTrigger = 50,
+				oldBSP = false,
+				prefix = "df_",
+				inputFile = bspFile,
+				outputDir = outputDir
+			};
+			
+			var converter = new BSPConverter(options, new DebugLogger());
+			converter.Convert();
+		}
+	}
+}
---
+++ BSPConvert.Test/DebugLogger.cs
@@ -0,0 +1,13 @@
+﻿using BSPConvertLib;
+using System.Diagnostics;
+
+namespace BSPConvert.Test
+{
+	public class DebugLogger : ILogger
+	{
+		public void Log(string message)
+		{
+			Debug.WriteLine(message);
+		}
+	}
+}
