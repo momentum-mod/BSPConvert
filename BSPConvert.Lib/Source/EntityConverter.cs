71100e1ec6ce370d088430544a9bb2937c7e2024
mac-tf2 <astokes96@hotmail.co.uk>
2023-06-21 15:32:35 +0100

    Ignore self-targeting entities

--- BSPConvert.Lib/Source/EntityConverter.cs
+++ BSPConvert.Lib/Source/EntityConverter.cs
@@ -215,6 +215,9 @@ namespace BSPConvert.Lib
 			var targets = GetTargetEntities(entity);
 			foreach (var target in targets)
 			{
+				if (entity == target)
+					return;
+
 				switch (target.ClassName)
 				{
 					case "target_delay":
@@ -447,6 +450,9 @@ namespace BSPConvert.Lib
 			var targets = GetTargetEntities(entity);
 			foreach (var target in targets)
 			{
+				if (entity == target)
+					return;
+
 				switch (target.ClassName)
 				{
 					case "target_stopTimer":
